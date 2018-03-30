6f2f9526d4f73b6b81551f8fd34f0a86e061d29c
fedeatanasoff <fede.atanasoff@gmail.com>
2018-03-29 22:12:24 -0300

    primer commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1 className='title'>My React App!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
